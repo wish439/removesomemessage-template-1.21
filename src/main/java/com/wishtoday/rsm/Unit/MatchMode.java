247ec3c3cd8511ad5f4e624a1b45210460fbafc1
wishtoday238 <wishtoday238@gmail.com>
2025-05-15 01:17:45 +0800

    枚举，豪用！

--- src/main/java/com/wishtoday/rsm/Unit/MatchMode.java
+++ src/main/java/com/wishtoday/rsm/Unit/MatchMode.java
@@ -1,22 +1,32 @@
 package com.wishtoday.rsm.Unit;
 
+import net.minecraft.text.Text;
+
 public enum MatchMode {
-    CONTAINS() {
+    CONTAINS("rsm.gui.contains") {
         @Override
         public boolean matches(String s, String p) {
             return s.contains(p) && !s.isEmpty();
         }
     },
-    PRECISION() {
+    PRECISION("rsm.gui.precision") {
         @Override
         public boolean matches(String s, String p) {
             return s.equals(p) && !s.isEmpty();
         }
     };
+    public final String TranslationKey;
+
+    MatchMode(String translationKey) {
+        TranslationKey = translationKey;
+    }
 
     public abstract boolean matches(String s, String p);
 
     public boolean equals(MatchMode mode) {
         return this == mode;
+    }
+    public Text getTranslation(){
+        return Text.translatable(this.TranslationKey);
     }
 }
